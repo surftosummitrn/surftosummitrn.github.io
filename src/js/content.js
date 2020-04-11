40ddd9e2a508b4145ade4373bd134d8dd1ddadec
g1eb <mail@g1eb.com>
2020-04-10 17:06:46 -0700

    Update the content section adding more video's to the list

--- src/js/content.js
+++ src/js/content.js
@@ -1,22 +1,63 @@
 'use strict';
 
 var content = [{
-  c1: "",
-  c2: "",
-  c3: "",
-  c4: "",
-  c5: "",
-  c6: "",
-  c7: "",
-  c8: "",
-  c9: "",
-  c10: "",
-  created_at: "2016-11-19T09:58:03.242Z",
-  locked: true,
-  title: "Test",
-  updated_at: "2017-02-05T19:01:07.161Z",
-  xpos: 2805.4490325772804,
-  ypos: 78.58518416734051,
-  yrot: 5.074503961941687,
-  zpos: -1062.758545301425,
+  src: 'https://www.youtube.com/embed/rxef5LJloSc',
+  width: '1000px',
+  height: '750px',
+  position: {
+    xpos: 2999.3454929117765,
+    ypos: 510.9838896724473,
+    zpos: -62.662701424466555,
+    yrot: 4.733278066665501,
+  },
+}, {
+  src: 'https://www.youtube.com/embed/Pdtf_J5RhGM',
+  width: '600px',
+  height: '400px',
+  position: {
+    xpos: 2547.142634003473,
+    ypos: 864.9466882476321,
+    zpos: -1584.9493373738637,
+    yrot: 5.269005445751684,
+  },
+}, {
+  src: 'https://www.youtube.com/embed/V5gw4Bghxa0',
+  width: '850px',
+  height: '650px',
+  position: {
+    xpos: 2834.6894840004197,
+    ypos: -604.1319945606538,
+    zpos: 982.1076974025985,
+    yrot: 4.37887093889664,
+  },
+}, {
+  src: 'https://www.youtube.com/embed/9ySEACEOA28',
+  width: '600px',
+  height: '400px',
+  position: {
+    xpos: 2822.677760503845,
+    ypos: 870.2694370983868,
+    zpos: 1016.1152790687662,
+    yrot: 4.36684867815627,
+  },
+}, {
+  src: 'https://www.youtube.com/embed/Rpz0ddk43TU',
+  width: '1000px',
+  height: '750px',
+  position: {
+    xpos: 2911.3511368744003,
+    ypos: -699.9414738742377,
+    zpos: -723.902312346174,
+    yrot: 4.956094986994154,
+  },
+}, {
+  src: 'https://www.youtube.com/embed/qTKekF6HZdQ',
+  width: '750px',
+  height: '500px',
+  position: {
+    xpos: 2490.900767228142,
+    ypos: 279.4443146646196,
+    zpos: 1671.9489728524177,
+    yrot: 4.121238874766633,
+  },
 }]
